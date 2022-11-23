15b32d6a1cde6bcefad84d66b85f7075f056fccc
AsafKed <asafkedem97@gmail.com>
2022-11-23 22:25:03 +0100

    API creates user playlists

--- API.py
+++ API.py
@@ -1,8 +1,8 @@
 from oauthlib.oauth2 import WebApplicationClient
 import credentials
 import requests
+import pandas as pd
 import json
-
 
 class API:
   def __init__(self):
@@ -10,20 +10,8 @@ class API:
       'Authorization': f'Bearer {credentials.ACCESS_TOKEN}'
     }
 
-    # The Spotify O Auth token has a time limit of 1 hour by default
-    # Starting with this guarantees a working token 
-    # self.refreshToken()
-    # Set up OAth stuff for the API to work
-    # client = WebApplicationClient(credentials.CLIENT_ID)
-    # self.url = client.prepare_request_uri(
-    #   credentials.AUTH_URL,
-    #   redirect_uri= credentials.REDIRECT_URI,
-    #   scope= credentials.SCOPE
-    # )
-    # print(self.url)
-
   def getPlaylists(self, user=credentials.USER_ID, offset=0, limit=20):
-    """Get the playlist of the specified user (default is the test user)
+    """Get the public playlists of the specified user (default is the test user)
 
     user: user ID from Spotify 
 
@@ -31,55 +19,60 @@ class API:
 
     limit: how many items to return
     """
-    self.url = f"https://api.spotify.com/v1/users/{user}/playlists?offset={offset}&limit={limit}"
+    url = f"https://api.spotify.com/v1/users/{user}/playlists?offset={offset}&limit={limit}"
 
     payload={}
     headers = {
       'Authorization': f'Bearer {credentials.ACCESS_TOKEN}'
     }
 
-    response = requests.request("GET", self.url, headers=headers, data=payload)
+    response = requests.request("GET", url, headers=headers, data=payload)
 
-    print(response.text)
+    response = json.loads(response.text)
+    return response
 
-  # def refreshToken(self):
-  #   client = WebApplicationClient(credentials.CLIENT_ID)
-  #   self.url = client.prepare_request_uri(
-  #     credentials.AUTH_URL,
-  #     redirect_uri= credentials.REDIRECT_URI,
-  #     scope= credentials.SCOPE
-  #   )
-  #   print(self.url)
+  def generatePlaylistNames(self):
+    """These are just the ids given in the data.
 
+    Returns: a list of strings.
+    """
+    users = pd.read_csv("participant data/survey data/msi_response.csv")
+    users = users["user_id"].unique()
+    return users
+
+    # TODO check if those playlists exist, if not, create them
+  def createUserPlaylists(self, user=credentials.USER_ID,):
+    """The user refers to the user account where the playlists will be created.
+    """
+    # Get names of playlists to generate
+    new_names = self.generatePlaylistNames()
+    existing_playlists = self.getPlaylists(limit=len(new_names))
+
+    # Only create playlists if no playlists with that name exists prior
+    playlist_names = []
+    for i in range(len(existing_playlists['items'])):
+        playlist_names.append(existing_playlists['items'][i]['name'])
     
-  #   test = client.prepare_refresh_token_request(self.url, refresh_token=credentials.REFRESH_TOKEN)
-  #   print(test)
-    # data = client.prepare_request_body(
-    #   # code = credentials.,
-    #   redirect_uri = credentials.REDIRECT_URI,
-    #   client_id = credentials.CLIENT_ID,
-    #   client_secret = credentials.CLIENT_SECRET
-    # )
-    # requests.request("POST", self.url, headers=self.headers, # TODO the rest of LHS)
+    creatables = [name for name in new_names if name not in playlist_names]
 
-    # spotify = OAuth2Session(credentials.CLIENT_ID, token=credentials.ACCESS_TOKEN)
-    # new_token = spotify.refresh_token()
+    # TODO REMOVE! The following line is for testing purposes only
+    creatables = ['python test']
+    
+    # Requests for the creation of the playlists
+    url = f"https://api.spotify.com/v1/users/{user}/playlists"
 
-  # # Check if playlist with user identifier already exists
-  # # For both playlists
+    for new_name in creatables:
+      payload = json.dumps({
+        "name": new_name,
+        "public": "true"
+      })
 
-  # # If user playlist does not exist, create one
-  # url = "https://api.spotify.com/v1/users/31gvr6zmqkzap3viblky5yd7ylhi/playlists"
+      headers = {
+        'Authorization': f'Bearer {credentials.ACCESS_TOKEN}',
+        'Content-Type': 'application/json'
+      }
 
-  # payload = json.dumps({
-  #   "name": "test-post",
-  #   "public": "true"
-  # })
-  # headers = {
-  #   'Authorization': 'Bearer BQDks8RcGKLJk8-xJptV_qNFsdbi24xUhjRmflJGBAHfX7xDTDONPvGLTxvo_gI2JUkszof7oAPySIcy3DVfbc3gPPex1uojvV9yUkpEE0_iuJb3wCjz6ucOYu-DEaBTptwFUgEA07H1VUz8AG_bZm658SUvmkGFT6J6JrD6uDgb1CoAx6Tt5i-CtVVSYyZ6LDIIMCy4nW-nfO7SEA6tT-uQm04SSaYkHG4HXxPdR80iSWteVFE7auVHIMeIY9Z4IHa5bSiCe-mdyQ',
-  #   'Content-Type': 'application/json'
-  # }
+      response = requests.request("POST", url, headers=headers, data=payload)
 
-  # response = requests.request("POST", url, headers=headers, data=payload)
+      print(response.text)
 
-  # print(response.text)
--- Postman_OAUTH_Instructions.md
+++ Postman_OAUTH_Instructions.md
@@ -12,10 +12,12 @@ Here are the instructions Asaf sent on Whatsapp. If you have further questions, 
 5. Copy the new access token and refresh tokens and put them aside for now
 
 **Git**
+
 6. Clone the repo if you haven't already
 7. Add the credentials file I'm about to send in the chat
 8. Put the credentials file in the repo's main directory (just in the highest level of the folder)
 9. Replace the old access token and refresh tokens with the new ones you just generated
 
 **Code**
+
 10. Now you should be able to make requests normally
