fa9791028fb47c4d61c9e52f998f0591b221a56d
AsafKed <asafkedem97@gmail.com>
2022-11-29 02:11:43 +0100

    Fix scope

--- implicit/app.py
+++ implicit/app.py
@@ -71,7 +71,7 @@ def auth():
         'client_id': CLIENT_ID,
         'response_type': 'token',
         'redirect_uri': REDIRECT_URI,
-        'scope': 'user-read-private user-read-email',
+        'scope': 'playlist-modify-public playlist-read-private playlist-modify-private',
         'state': state,
     }
 
